a835ee9732fdcf61cdd8c577b5485abb82c4f974
Ariel Zuniga <azuniga2003@gmail.com>
2019-10-29 21:58:59 -0600

    update employee_db.sql

--- EmployeeSQL/employees_db.sql
+++ EmployeeSQL/employees_db.sql
@@ -65,3 +65,32 @@ CREATE TABLE dept_manager(
 SELECT * FROM dept_manager;
 
 
+--List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+salaries.emp_no=employees.emp_no;
+
+--List employees who were hired in 1986.
+SELECT * FROM employees WHERE hire_date >= date'1986-01-01'
+AND hire_date <= date'1986-12-31';
+
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, 
+--last name, first name, and start and end employment dates.
+SELECT dept_manager.dept_no, departments.dept_name, employees.last_name, 
+employees.first_name, dept_manager.from_date, dept_manager. to_date
+FROM dept_manager
+INNER JOIN departments ON
+departments.dept_no=dept_manager.dept_no 
+INNER JOIN employees on dept_manager.emp_no =employees.emp_no;
+
+--List all employees whose first name is "Hercules" and last names begin with "B."
+Select * FROM employees WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--Rename employees column 
+ALTER TABLE employees
+RENAME COLUMN lats_name TO last_name;
+
+
