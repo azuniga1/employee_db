f7255c1958d76ca28c94675f7d7c1f617289aac6
Ariel Zuniga <azuniga2003@gmail.com>
2019-10-31 21:46:36 -0600

    update SQL file

--- EmployeeSQL/employees_db.sql
+++ EmployeeSQL/employees_db.sql
@@ -1,70 +1,3 @@
-CREATE TABLE employees (
-    emp_no SERIAL PRIMARY KEY,
-    birt_date date,
-    first_name varchar(30),
-    lats_name varchar(30),
-    gender varchar(5),
-    hire_date date
-);
-
-SELECT * FROM employees;
-
-DROP TABLE departments
-
-CREATE TABLE departments(
-    dept_no VARCHAR(5) NOT NULL PRIMARY KEY,
-    dept_name varchar(30) NOT NULL
- );
-
-SELECT * FROM departments;
-
-CREATE TABLE dept_emp(
-    id SERIAL PRIMARY KEY,
-    emp_no int,
-    dept_no VARCHAR(5),
-    from_date date,
-    to_date date,
-   	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
-
-SELECT * FROM dept_emp;
-
-CREATE TABLE salaries(
-    id SERIAL PRIMARY KEY,
-    emp_no INT,
-    salary MONEY NOT NULL,
-    from_date DATE,
-    to_date DATE,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-SELECT * FROM salaries;
-
-CREATE TABLE titles(
-    id SERIAL PRIMARY KEY,
-    emp_no int,
-    title varchar(30),
-    from_date date,
-    to_date date,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-SELECT * FROM titles;
-
-CREATE TABLE dept_manager(
-    id SERIAL PRIMARY KEY,
-    dept_no VARCHAR(5),
-    emp_no int,
-    from_date date,
-    to_date date,
- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
-
-SELECT * FROM dept_manager;
-
-
 --List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
 FROM employees
@@ -75,7 +8,6 @@ salaries.emp_no=employees.emp_no;
 SELECT * FROM employees WHERE hire_date >= date'1986-01-01'
 AND hire_date <= date'1986-12-31';
 
-
 --List the manager of each department with the following information: department number, department name, the manager's employee number, 
 --last name, first name, and start and end employment dates.
 SELECT dept_manager.dept_no, departments.dept_name, employees.last_name, 
@@ -85,12 +17,48 @@ INNER JOIN departments ON
 departments.dept_no=dept_manager.dept_no 
 INNER JOIN employees on dept_manager.emp_no =employees.emp_no;
 
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+Select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN  departments ON 
+departments.dept_no= dept_emp.dept_no;
+
 --List all employees whose first name is "Hercules" and last names begin with "B."
 Select * FROM employees WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
---Rename employees column 
-ALTER TABLE employees
-RENAME COLUMN lats_name TO last_name;
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+Select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN  departments ON 
+departments.dept_no= dept_emp.dept_no
+WHERE dept_name = 'Sales';
 
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+Select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN  departments ON 
+departments.dept_no= dept_emp.dept_no
+WHERE dept_name IN ('Sales', 'Development');
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Last_name_count" 
+FROM employees
+GROUP BY last_name
+ORDER BY last_name DESC;
+
+--Bonus 'Search your ID' Funny! 
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+salaries.emp_no=employees.emp_no
+WHERE employees.emp_no = 499942;
 
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,68 @@
+CREATE TABLE employees (
+    emp_no SERIAL PRIMARY KEY,
+    birt_date date,
+    first_name varchar(30),
+    lats_name varchar(30),
+    gender varchar(5),
+    hire_date date
+);
+
+SELECT * FROM employees;
+
+--Rename employees column 
+ALTER TABLE employees
+RENAME COLUMN lats_name TO last_name;
+
+
+CREATE TABLE departments(
+    dept_no VARCHAR(5) NOT NULL PRIMARY KEY,
+    dept_name varchar(30) NOT NULL
+ );
+
+SELECT * FROM departments;
+
+CREATE TABLE dept_emp(
+    id SERIAL PRIMARY KEY,
+    emp_no int,
+    dept_no VARCHAR(5),
+    from_date date,
+    to_date date,
+   	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE salaries(
+    id SERIAL PRIMARY KEY,
+    emp_no INT,
+    salary MONEY NOT NULL,
+    from_date DATE,
+    to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
+
+CREATE TABLE titles(
+    id SERIAL PRIMARY KEY,
+    emp_no int,
+    title varchar(30),
+    from_date date,
+    to_date date,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM titles;
+
+CREATE TABLE dept_manager(
+    id SERIAL PRIMARY KEY,
+    dept_no VARCHAR(5),
+    emp_no int,
+    from_date date,
+    to_date date,
+ 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_manager;
